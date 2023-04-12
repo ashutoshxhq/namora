3f69f7d5aa53a4f3db85b06922ff409124695bb1
Ashutosh Dubey <ashutoshxhq@gmail.com>
2023-04-12 13:00:25 +0530

    added openai message handler

--- apps/chat-service/src/modules/chat/service.rs
+++ apps/chat-service/src/modules/chat/service.rs
@@ -1,10 +1,11 @@
-use crate::{error::Error};
+use async_openai::{types::{ChatCompletionRequestMessage, ChatCompletionRequestMessageArgs, Role, CreateChatCompletionRequestArgs}, Client};
 
-use super::dto::MessageWithConversationContext;
+use crate::error::{Error, ExecuteAIError};
+
+use super::dto::{MessageWithConversationContext, Message};
 
 #[derive(Clone)]
-pub struct ChatService {
-}
+pub struct ChatService {}
 
 impl ChatService {
     pub fn new() -> Self {
@@ -15,7 +16,58 @@ impl ChatService {
         &self,
         message_with_context: MessageWithConversationContext,
     ) -> Result<MessageWithConversationContext, Error> {
-        Ok(message_with_context)
+        let client = Client::new().with_api_key(std::env::var("OPENAI_API_KEY")?);
+        let mut messages: Vec<ChatCompletionRequestMessage> = Vec::new();
+        if let Some(ai_system_prompt) = message_with_context.ai_system_prompt {
+            messages.push(
+                ChatCompletionRequestMessageArgs::default()
+                    .role(Role::System)
+                    .content(ai_system_prompt)
+                    .build()?,
+            );
+            for message in &message_with_context.context.history.messages {
+                if message.message_from == "AI" {
+                    messages.push(
+                        ChatCompletionRequestMessageArgs::default()
+                            .role(Role::Assistant)
+                            .content(serde_json::to_value(message)?.to_string())
+                            .build()?,
+                    )
+                } else {
+                    messages.push(
+                        ChatCompletionRequestMessageArgs::default()
+                            .role(Role::User)
+                            .content(message.message.clone())
+                            .build()?,
+                    )
+                }
+            }
+            messages.push( ChatCompletionRequestMessageArgs::default()
+            .role(Role::User)
+            .content(serde_json::to_value(message_with_context.message)?.to_string())
+            .build()?);
+
+            let request = CreateChatCompletionRequestArgs::default()
+                .model("gpt-4")
+                .messages(messages)
+                .build()?;
+
+            let response = client.chat().create(request).await?;
+            let message: Message = serde_json::from_str(&(response.choices[0].message.content))?;
+            let mut conversation_context = message_with_context.context;
+            conversation_context.history.messages.push(message.clone());
+            let context_with_new_message = MessageWithConversationContext{
+                ai_system_prompt: None,
+                context: conversation_context,
+                message
+            };
+            return Ok(context_with_new_message);
+        }
+        Err(ExecuteAIError::new(
+            "NO_SYSTEM_PROMPT".to_string(),
+            "No system prompt provided in message".to_string(),
+            400
+        ))
     }
 
     pub async fn chat_system(
