382707a5d13e47e8476a5665a41c3ee54e4165cf
Ashutosh Dubey <ashutoshxhq@gmail.com>
2023-05-05 11:25:38 +0530

    added access token api

---
+++ apps/app.namora.ai/src/pages/api/accessToken.ts
@@ -0,0 +1,7 @@
+import { getAccessToken } from "@auth0/nextjs-auth0";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const { accessToken } = await getAccessToken(req, res, {});
+    res.status(200).json({ accessToken });
+}
