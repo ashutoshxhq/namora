195264f0e0d8829d1e8f6938e5faac1f2278a2ee
Ashutosh Dubey <ashutoshxhq@gmail.com>
2023-05-10 10:06:31 +0530

    UI: fixed websockets issue

--- apps/web-console/src/components/chats/window.tsx
+++ apps/web-console/src/components/chats/window.tsx
@@ -9,6 +9,7 @@ import { getTimeInShortMinuteFormat } from "@/utils/date";
 import { BASE_WS_URL } from "@/web-sockets/constants";
 import { FormInputTextField } from "@/design-system/form";
 import { useScrollToBottom } from "@/hooks";
+import { SWRSubscriptionOptions } from "swr/subscription";
 
 const schema = yup.object().shape({
   message: yup
@@ -17,77 +18,9 @@ const schema = yup.object().shape({
     .min(1, "Minimum one character is required"),
 });
 
-const encoder = new TextEncoder();
-const _messages = [
-  {
-    id: "1",
-    content: "Hello",
-    sender: false,
-    created_at: new Date(),
-  },
-  {
-    id: "2",
-    content: "Hi! How are you?",
-    sender: true,
-    created_at: new Date(),
-  },
-  {
-    id: "3",
-    content: "Hello",
-    sender: false,
-    created_at: new Date(),
-  },
-  {
-    id: "4",
-    content: "Hi! How are you?",
-    sender: true,
-    created_at: new Date(),
-  },
-  {
-    id: "5",
-    content: "Hello",
-    sender: false,
-    created_at: new Date(),
-  },
-  {
-    id: "6",
-    content: "Hi! How are you?",
-    sender: true,
-    created_at: new Date(),
-  },
-  {
-    id: "7",
-    content:
-      "HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello",
-    sender: false,
-    created_at: new Date(),
-  },
-  {
-    id: "8",
-    content:
-      "Hi! How are you?Hi! How are you?Hi! How are you?Hi! How are you?Hi! How are you?Hi! How are you?Hi! How are you?Hi! How are you?Hi! How are you?Hi! How are you?Hi! How are you?Hi! How are you?Hi! How are you?Hi! How are you?Hi! How are you?Hi! How are you?Hi! How are you?Hi! How are you?Hi! How are you?Hi! How are you?",
-    sender: true,
-    created_at: new Date(),
-  },
-  {
-    id: "9",
-    content: "Hello",
-    sender: false,
-    created_at: new Date(),
-  },
-  {
-    id: "10",
-    content: "Hi! How are you?",
-    sender: true,
-    created_at: new Date(),
-  },
-];
-
-const isBrowser = typeof window !== "undefined";
-
 export const Window = (props: any) => {
   const { accessToken, user } = props;
-  const { namora_team_id, namora_user_id, name: userName } = user;
+  const { name: userName } = user;
   const webSocketRef = useRef<any>(null);
   const [messages, setMessages] = useState<any[]>([]);
   const [count, setCount] = useState(1);
@@ -121,85 +54,67 @@ export const Window = (props: any) => {
     };
 
     const msgWithProps = {
-      Data: {
-        content: msg,
-        session_id: "8d77a6ab-975b-49c6-871e-707798a22751",
-        context: {
-          user_id: namora_user_id,
-          team_id: namora_team_id,
-          authorization_token: accessToken,
-        },
-      },
+      content: msg,
+      message_type: "query",
+      reciever: "system",
     };
 
     if (accessToken) {
       let data = JSON.stringify(msgWithProps);
 
-      const binaryData = encoder.encode(data);
-      webSocketRef?.current?.send(binaryData.buffer);
+      webSocketRef?.current?.send(data);
       setCount((prevValue) => prevValue + count);
       timeoutRef.current = setTimeout(scrollToBottom, 500);
       setMessages((prev) => [
         ...prev,
-        { ...msgWithProps.Data, ...defaultProps },
+        { ...msgWithProps, ...defaultProps },
       ]);
       reset();
     }
   };
 
   const { handleSubmit } = hookFormProps;
-  const subscribeWS: SWRSubscription<string, number, Error> = (
-    key,
-    { next }
+  const subscribeWS = (
+    key: string,
+    { next }: SWRSubscriptionOptions<number, Error>
   ) => {
-    const now = new Date();
-    const nowTime = now.getTime();
-    const webSocket = isBrowser ? new WebSocket(BASE_WS_URL) : null;
-    webSocketRef.current = webSocket;
-    if (webSocket) {
+    const socket = new WebSocket(key);
+    webSocketRef.current = socket;
+    if (socket) {
       // webSocket.addEventListener("message", (event) => next(null, event.data));
       // webSocket.addEventListener("error", (event: any) => next(event.error));
 
-      webSocket.addEventListener("open", (event: any) => {
-        let data = JSON.stringify({
-          Data: {
-            content: "hello",
-            context: {
-              session_id: "abc",
-              user_id: namora_user_id,
-              team_id: namora_team_id,
-              authorization_token: accessToken,
-            },
-          },
-        });
-
-        const binaryData = encoder.encode(data);
-        webSocket?.send(binaryData.buffer);
+      socket.addEventListener("open", (event: any) => {
+        console.log("Connected to User Socket Server");
+        // next(null, event.data);
       });
 
-      const receiveWSMessage = async (event: any) => {
-        let data = JSON.parse(await event.data.text());
+      socket.addEventListener("message", async (event: any) => {
+        console.log("Message from server ", event.data)
+        let data = JSON.parse(await event.data);
 
         const defaultProps = {
-          id: `from-user-${count}-${nowTime}`,
+          id: `from-user-${count}-${new Date().getTime()}`,
           order: count,
-          timestamp: now,
-          type: "FROM_AI",
+          timestamp: new Date(),
+          type: "system",
           sender: false,
         };
 
-        setMessages((prev) => [...prev, { ...data.Data, ...defaultProps }]);
-      };
-      webSocket.addEventListener("message", receiveWSMessage);
+        setMessages((prev) => [...prev, { ...data, ...defaultProps, content: data.content.replaceAll("\n", `<br/>`) }]);
+        setCount((prevValue) => prevValue + count);
+        next(null, event.data)
+      });
+
       timeoutRef.current = setTimeout(scrollToBottom, 500);
     }
 
     return () => {
-      webSocket?.close();
+      socket?.close();
     };
   };
 
-  useSWRSubscription(BASE_WS_URL, subscribeWS);
+  useSWRSubscription(BASE_WS_URL+ `?token=${accessToken}`, subscribeWS);
 
   useEffect(() => {
     const timeout = timeoutRef.current;
@@ -225,7 +140,7 @@ export const Window = (props: any) => {
 
   if (!Object.keys(messageGroupData).length)
     return (
-      <div className="box-border overflow-auto bg-white  divide-gray-200 rounded-lg shadow h-[calc(100vh_-_140px)] mb-2 p-2">
+      <div className="overflow-auto h-[calc(100vh_-_140px)] mb-2 p-2 pb-40">
         <div className="flex items-center justify-center w-full h-full">
           <EmptyState />
         </div>
@@ -235,7 +150,7 @@ export const Window = (props: any) => {
   return (
     <>
       <div
-        className="box-border overflow-auto bg-white  divide-gray-200 rounded-lg  h-[calc(100vh_-_140px)] mb-2 p-2 pb-40 shadow-inset border-black"
+        className="overflow-auto h-[calc(100vh_-_140px)] mb-2 p-2 pb-40"
         ref={divRef}
       >
         {Object.keys(messageGroupData).map(
--- apps/web-console/src/design-system/form/text.tsx
+++ apps/web-console/src/design-system/form/text.tsx
@@ -37,8 +37,8 @@ export const FormInputTextField = ({
               type="text"
               className={`${
                 isError
-                  ? "block w-full rounded-md border-0 px-1.5 py-1.5 pr-10 text-red-900 ring-1 ring-inset ring-red-300 placeholder:text-red-300 focus:ring-1 focus:ring-inset focus:outline-0 focus:ring-red-500 sm:text-sm sm:leading-6"
-                  : "block w-full rounded-md border-0 px-1.5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:outline-0 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  ? "block w-full rounded-md border-0 px-2 py-1.5 pr-10 text-red-900 ring-1 ring-inset ring-red-300 placeholder:text-red-300 focus:ring-1 focus:ring-inset focus:outline-0 focus:ring-red-500 sm:text-sm sm:leading-6"
+                  : "block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:outline-0 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               }`}
               placeholder={errMessage || placeholder}
               aria-invalid="true"
