14bad819bd33106a16dd5ed2bdda9be44fd403ec
Ashutosh Dubey <ashutoshxhq@gmail.com>
2023-05-24 10:40:37 +0530

    added paginated tasks

--- apps/engine-service/src/modules/tasks/service.rs
+++ apps/engine-service/src/modules/tasks/service.rs
@@ -6,6 +6,8 @@ use engine_db_repository::{
 use namora_core::types::{db::DbPool, error::Error};
 use uuid::Uuid;
 
+use super::types::{PaginatedResult, MetaData};
+
 #[derive(Clone)]
 pub struct TaskService {
     pub pool: DbPool,
@@ -27,17 +29,29 @@ impl TaskService {
         offset: Option<i64>,
         limit: Option<i64>,
         team_id: Uuid,
-    ) -> Result<Vec<Task>, Error> {
+    ) -> Result<PaginatedResult<Vec<Task>>, Error> {
         let mut conn = self.pool.clone().get()?;
         let offset = if let Some(offset) = offset { offset } else { 0 };
         let limit = if let Some(limit) = limit { limit } else { 10 };
+
+        let total = dsl::tasks
+            .filter(dsl::team_id.eq(team_id))
+            .count()
+            .get_result(&mut conn)?;
 
         let results: Vec<Task> = dsl::tasks
             .filter(dsl::team_id.eq(team_id))
             .offset(offset)
             .limit(limit)
             .load(&mut conn)?;
-        Ok(results)
+        let res_len = results.len();
+        
+        Ok(PaginatedResult { data: results, meta_data: MetaData{
+            current_limit: limit,
+            current_offset: offset,
+            result_count: res_len as i64,
+            total
+        } })
     }
 
     pub async fn create_task(&self, data: CreateTask) -> Result<Task, Error> {
--- apps/engine-service/src/modules/tasks/types.rs
+++ apps/engine-service/src/modules/tasks/types.rs
@@ -1,7 +1,22 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize)]
 pub struct GetTasksByQuery {
     pub offset: Option<i64>,
     pub limit: Option<i64>,
+}
+
+
+#[derive(Deserialize, Serialize)]
+pub struct MetaData {
+    pub total: i64,
+    pub current_offset: i64,
+    pub current_limit: i64,
+    pub result_count: i64,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct PaginatedResult<T> {
+    pub data: T,
+    pub meta_data: MetaData,
 }
